4f1ff420b3e82a28a4db7f5341f6a4b7b589cdd1
M Sami <beboolean@gmail.com>
2023-12-16 01:00:58 +0530

    navbar setup

--- app/page.js
+++ app/page.js
@@ -1,11 +1,33 @@
-import Image from 'next/image';
-import UserMenu from '@/components/UserMenu';
+import Image from "next/image";
+import Link from "next/link";
+import UserMenu from "@/components/UserMenu";
+import logo from "../assets/Logo.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserMenu />
-
+    <main className="text-black font-poppins bg-gradient-custom flex justify-between flex-row p-4 h-screen">
+      <div className="text-white p-14 flex w-229 h-785 flex-col gap-1 nav-gradient-custom justify-between rounded-2xl">
+        <div className="flex flex-col gap-6 ">
+        <div className="content-center">
+          <Image src={logo} width="90" height="90" alt="logo image" />
+        </div>
+        <div>
+          <ul>
+            <li className="flex text-dash-font flex-col gap-8 w-full">
+              <Link href="">Dashboard</Link>
+              <Link href="/pointsgrabber">Points Grabber</Link>
+              <Link href="dailydairy">Daily Dairy</Link>
+              <Link href="marketplace">Points Marketplace</Link>
+             
+            </li>
+          </ul>
+          </div>
+        </div>
+        <div> <p>Log Out</p></div>
+      </div>
+      <div className="p-8 w-4/5">
+        <UserMenu />
+      </div>
     </main>
   );
 }
