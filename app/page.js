5ff11e10bd5141b89aa98de41c1153ee5e5a925c
M Sami <beboolean@gmail.com>
2023-12-16 18:40:08 +0530

    components setup and side navbar done

--- app/page.js
+++ app/page.js
@@ -1,33 +1,44 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import { useState } from "react";
+
 import UserMenu from "@/components/UserMenu";
-import logo from "../assets/Logo.png";
+import Navbar from "@/components/Navbar";
+
+import Dashboard from "@/components/Dashboard";
+import PointsGrabber from "@/components/PointsGrabber";
+import MarketPlace from "@/components/MarketPlace";
+import DailyDairy from "@/components/DailyDairy";
+
 
 export default function Home() {
+  const [render,setRender]=useState('dashboard');
+
+  const handleNavItemClick = (component) => {
+    setRender(component);
+  };
+
   return (
-    <main className="text-black font-poppins bg-gradient-custom flex justify-between flex-row p-4 h-screen">
-      <div className="text-white p-14 flex w-229 h-785 flex-col gap-1 nav-gradient-custom justify-between rounded-2xl">
-        <div className="flex flex-col gap-6 ">
-        <div className="content-center">
-          <Image src={logo} width="90" height="90" alt="logo image" />
-        </div>
-        <div>
-          <ul>
-            <li className="flex text-dash-font flex-col gap-8 w-full">
-              <Link href="">Dashboard</Link>
-              <Link href="/pointsgrabber">Points Grabber</Link>
-              <Link href="dailydairy">Daily Dairy</Link>
-              <Link href="marketplace">Points Marketplace</Link>
-             
-            </li>
-          </ul>
-          </div>
-        </div>
-        <div> <p>Log Out</p></div>
+   
+   <main className="text-black  bg-gradient-custom font-bold font-poppins  flex justify-between flex-row p-4 h-screen">
+
+     <Navbar onNavItemClick={handleNavItemClick} render={render} />
+
+
+
+      {/* *******************right section start ******************** */}
+
+    <div className="p-8 w-4/5 flex flex-col">
+    
+      <UserMenu />
+   
+     <section>
+      {render === 'dashboard' && <Dashboard/>}
+      {render === 'marketplace' && <MarketPlace/>}
+      {render === 'dailydairy' && <DailyDairy/>}
+      {render === 'pointsgrabber' && <PointsGrabber/>}
+      </section>
       </div>
-      <div className="p-8 w-4/5">
-        <UserMenu />
-      </div>
+
     </main>
   );
 }
--- components/UserMenu.jsx
+++ components/UserMenu.jsx
@@ -13,16 +13,18 @@ const UserMenu = () => {
 
   return (
     <>
-	<div className="flex items-center justify-between gap-10 ">
-	<p>dsd</p>
-	<div className="flex font-bold text-sm items-center gap-6">
-        <p>{/*{user.points} */} 100 points</p>
-      <div className="flex items-center gap-2">
-        <UserButton afterSignOutUrl="/" />
-        <p >{user.username}</p>
-      </div>
-	  </div>
-	  </div>
+     
+        <div className="flex items-center justify-between gap-10 ">
+          <p className="">KidZelp</p>
+          <div className="flex font-bold text-sm items-center gap-6">
+            <p>{/*{user.points} */} 100 points</p>
+            <div className="flex items-center gap-2">
+              <UserButton afterSignOutUrl="/" />
+              <p>{user.username}</p>
+            </div>
+          </div>
+        </div>
+   
     </>
   );
 };
