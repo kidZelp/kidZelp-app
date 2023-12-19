81a1b20fb6fc2aa1ff66b3b5913e2e6b35d80c75
M Sami <beboolean@gmail.com>
2023-12-19 15:15:49 +0530

    dashboard

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import student from "../assets/5. College Student.png";
+import Image from "next/image";
+import cap from "../assets/Scholarcap scroll.png";
 
 const Dashboard = () => {
   return (
-    <div>
-     Dashboard
-     
-    </div>
-  )
-}
+    <>
+      <div className="flex mt-6 justify-between dashboard-gradient rounded-xl p-5 text-white">
+        <div className="flex flex-col">
+          <p className="text-sm"> December 14,2023</p>
+          <p >Welcome back, Saksham! </p>
+          <p>Always stay updated in your student portal</p>
+        </div>
+        <div className="flex ">
+          <Image src={cap} width={150} alt="img" />
+          <Image src={student} width={130} alt="img" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
