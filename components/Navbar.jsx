942aec94e39c086f0ea1e5109094a1cd78774a17
M Sami <beboolean@gmail.com>
2023-12-20 17:30:32 +0530

    done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import dsh from "../assets/dsh.svg";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -14,13 +15,13 @@ const Navbar = ({ onNavItemClick, render }) => {
   };
 
   return (
-    <div className=" text-white p-6 md:flex w-56 items-center h-screen flex-col gap-1 nav-gradient-custom justify-between rounded-2xl">
+    <div className=" text-white p-6 md:flex w-56 items-center h-auto flex-col gap-1 nav-gradient-custom justify-between rounded-2xl">
       <div className="flex flex-col gap-6 ">
         <div>
-          <Image src={logo} width="90" height="auto" alt="logo image" />
+          <Image src={dsh} width="200" height="auto" alt="logo image" />
         </div>
         <div>
-          <ul>
+          <ul className="flex flex-col gap-5">
             <li className="flex p-1 bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border  text-dash-font flex-col gap-8 w-full">
               <Link
                 href=""
@@ -34,74 +35,65 @@ const Navbar = ({ onNavItemClick, render }) => {
                   height="auto"
                   width="13"
                   alt="Dashboard Icon"
-				
                 />
                 Dashboard
               </Link>
-              {/* <Link
-								href=''
-								onClick={() =>
-									handleItemClick("pointsgrabber")
-								}
-								className={`flex gap-1 ${
-									render === "pointsgrabber"
-										? "text-white"
-										: "text-gray-300"
-								}`}
-							>
-								<Image
-									src={icon}
-									height='auto'
-									width='13'
-									alt='Icon'
-								/>
-								Points Grabber
-							</Link> */}
-              {/* <Link
-								href=''
-								onClick={() =>
-									handleItemClick("dailydairy")
-								}
-								className={`flex gap-1 ${
-									render === "dailydairy"
-										? "text-white"
-										: "text-gray-300"
-								}`}
-							>
-								<Image
-									src={icon}
-									height='auto'
-									width='13'
-									alt='Icon'
-								/>
-								Daily Dairy
-							</Link> */}
-              {/* <Link
-								href=''
-								onClick={() =>
-									handleItemClick("marketplace")
-								}
-								className={`flex gap-1 ${
-									render === "marketplace"
-										? "text-white"
-										: "text-gray-300"
-								}`}
-							>
-								<Image
-									src={icon}
-									height='auto'
-									width='13'
-									alt='Icon'
-								/>
-								Points Marketplace
-							</Link> */}
+            </li>
+            <li className="flex p-1 bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border text-dash-font flex-col gap-8 w-full">
+              <Link
+                href=""
+                onClick={() => handleItemClick("dashboard")}
+                className={`flex gap-1 ${
+                  render === "dashboard" ? "text-white" : "text-gray-300"
+                }`}
+              >
+                <Image
+                  src={dashboard}
+                  height="auto"
+                  width="13"
+                  alt="Dashboard Icon"
+                />
+                Play
+              </Link>
+            </li>
+            <li className="flex p-1 bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border  text-dash-font flex-col gap-8 w-full">
+              <Link
+                href=""
+                onClick={() => handleItemClick("dashboard")}
+                className={`flex gap-1 ${
+                  render === "dashboard" ? "text-white" : "text-gray-300"
+                }`}
+              >
+                <Image
+                  src={dashboard}
+                  height="auto"
+                  width="13"
+                  alt="Dashboard Icon"
+                />
+                Fun Activities
+              </Link>
+            </li>
+            <li className="flex p-1 bg-gray-800 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border  text-dash-font flex-col gap-8 w-full">
+              <Link
+                href=""
+                onClick={() => handleItemClick("dashboard")}
+                className={`flex gap-1 ${
+                  render === "dashboard" ? "text-white" : "text-gray-300"
+                }`}
+              >
+                <Image
+                  src={dashboard}
+                  height="auto"
+                  width="13"
+                  alt="Dashboard Icon"
+                />
+                Chatbot
+              </Link>
             </li>
           </ul>
         </div>
       </div>
-      <div>
-	Contact Us: +91875046798 , kidzelp@gmail.com
-	  </div>
+      <div>Contact Us: +91875046798 , kidzelp@gmail.com</div>
     </div>
   );
 };
