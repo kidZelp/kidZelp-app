9e225696814ec180060bfbc9ce50b68c45ec4967
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-12-20 15:03:36 +0530

    Refactor value mapping in GuardianPage component

--- app/guardian/page.js
+++ app/guardian/page.js
@@ -30,30 +30,30 @@ export default function GuardianPage() {
       postText: "Observant",
     },
     {
-        preText:"Thinking",
-        value:50,
-        postText:"Feeling"
+      preText: "Thinking",
+      value: 50,
+      postText: "Feeling"
     },
-   
+
   ];
 
-  const values = report.map((value) => {
+  const values = report.map((value, i) => {
     return (
-      <Fragment>
-        <div className="flex gap-4 items-center">
-          <span>{value.preText}</span>
 
-   
-   <div className="w-1/2 relative">
-      <div className="text-center text-blue-500 text-lg">{`${value.value}%`}</div>
-      <div className="bg-gray-300 h-2 rounded overflow-hidden relative">
-        <div className="bg-blue-500 h-full transition-width" style={{ width: `${value.value}%` }}></div>
-      </div>
-    </div>
+      <div className="flex gap-4 items-center" key={i}>
+        <span>{value.preText}</span>
 
-          <span>{value.postText}</span>
+
+        <div className="w-1/2 relative">
+          <div className="text-center text-blue-500 text-lg">{`${value.value}%`}</div>
+          <div className="bg-gray-300 h-2 rounded overflow-hidden relative">
+            <div className="bg-blue-500 h-full transition-width" style={{ width: `${value.value}%` }}></div>
+          </div>
         </div>
-      </Fragment>
+
+        <span>{value.postText}</span>
+      </div>
+
     );
   });
 
@@ -64,9 +64,31 @@ export default function GuardianPage() {
 
         <div className="flex  bg-white w-full h-96 gap-5 p-10 rounded-xl flex-col justify-center border-solid">
           <div>Final Report and Statistics</div>
-          <div className="flex  flex-col gap-3">{values}</div>
+          <ValueMapper report={report} />
         </div>
       </div>
     </Fragment>
   );
 }
+
+
+const ValueMapper = ({ report }) => {
+  return (
+    <div className="flex flex-col gap-3 justify-center">
+      {report.map((value, i) => (
+        <div className="flex gap-4 items-center " key={i}>
+          <span className='w-[10%]'>{value.preText}</span>
+          <span className='w-[10%] text-center text-blue-500 text-lg'>{value.value}</span>
+          <div className="w-1/2 relative">
+            {/* <div className="text-center text-blue-500 text-lg">{`${value.value}%`}</div> */}
+            <div className="bg-gray-300 h-2 rounded overflow-hidden relative">
+              <div className="bg-blue-500 h-full transition-width" style={{ width: `${value.value}%` }}></div>
+            </div>
+          </div>
+
+          <span>{value.postText}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
